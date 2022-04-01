8db0ed487ee7eaabe713e4926e7878bd3c9468d5
pralr <101532384+pralr@users.noreply.github.com>
2022-04-01 09:52:27 -0300

    Update filter.c

--- Aula 02 - Ponteiros para Funções/filter.c
+++ Aula 02 - Ponteiros para Funções/filter.c
@@ -2,42 +2,51 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-void filter(bool(*f)(int x), int n, int i, int *array) {
+// atualizar: realocar toda vez que adicionar na funcao
+
+void filter(bool(*f)(int x), int n, int i, int newsize, int *array, int *array_f) {
     if(i < n) {
-        if(f(*(array+i)) == true)  
-            printf("%d ", *(array+i));
+        if(f(*(array+i)) == true)  {
+            array_f[newsize] = array[i];
+            newsize++; 
+        }
         i++; 
-        filter(f, n, i, array);
+        filter(f, n, i, newsize, array, array_f);
+    } else {
+        for(i = 0; i < newsize; i++) 
+            printf("%d ", *(array_f+i));
     }
 }
 
 bool even(int x) {
     if(x % 2 == 0) 
         return 1;
-    else return 0; 
+    return 0; 
 }
 
 bool odd(int x) {
     if(x % 2 != 0)
         return 1;
-    else return 0; 
+    return 0; 
 }
 
 int main() {
-    int n, *array;
+    int n, i, *array, *array_f;
     printf("Digite o tamanho do array: \n");
     scanf("%d", &n); 
     array = (int*)malloc(n*sizeof(int));
+    array_f = (int*)malloc(n*sizeof(int));
 
-    for(int i = 0; i < n; i++)
+    for(i = 0; i < n; i++)
         scanf("%d", (array+i));
 
     printf("---------PAR-------------\n");
-    filter(even, n, 0, array);
+    filter(even, n, 0, 0, array, array_f); 
 
     printf("\n---------IMPAR----------\n");
-    filter(odd, n, 0, array);
+    filter(odd, n, 0, 0, array, array_f); 
 
     free(array); 
+    free(array_f);
     return 0;
 }

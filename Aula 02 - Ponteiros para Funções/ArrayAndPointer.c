32e2e93f221a36f2fbc16e086c5f28809ee04dfc
pralr <101532384+pralr@users.noreply.github.com>
2022-04-02 16:12:33 -0300

    is the array a pointer?

--- Aula 02 - Ponteiros para Funções/ArrayAndPointer.c
+++ Aula 02 - Ponteiros para Funções/ArrayAndPointer.c
@@ -7,11 +7,6 @@ do tipo void que recebe o array criado e um inteiro n que multiplica cada elemen
 
 #include <stdio.h>
 
-void multiply(int array[]) {
-    for (int i = 0; i < 3; i++) 
-        array[i] = array[i] * 2; 
-}
-
 int main() {
     
     int array[3] = {1, 2, 3};
@@ -25,14 +20,6 @@ int main() {
         printf("%d ", *(array + i));
         
     printf("\n");
-    
-    // chame a função para mudar os valores do array
-    multiply(array);
-    
-    for(int i = 0; i < 3; i++)
-        printf("%d ", *(array + i));
-
-    // os elementos foram alterados na main
 
 /* R: Não. Internamente, funcionam como ponteiros (quando você faz array[i] está fazendo *(array + i), no entanto, é importante lembrar 
 que o array é uma estrutura de dados homogênea e um ponteiro é uma variável que armazena o endereço de uma variável. Embora muitas vezes
